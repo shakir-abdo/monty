3daeb42f2649d2c1473bc72d2f56b3963f4dd2e4
awab228 <awab228@yahoo.com>
2023-11-16 23:10:18 +0000

    Implement pstr

--- execute.c
+++ execute.c
@@ -22,6 +22,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 		{"mul", f_mul},
 		{"mod", f_mod},
 		{"pchar", f_pchar},
+		{"pstr", f_pstr},
 		{NULL, NULL}};
 	unsigned int i = 0;
 	char *op;
--- monty.h
+++ monty.h
@@ -67,6 +67,7 @@ void f_div(stack_t **head, unsigned int counter);
 void f_mul(stack_t **head, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
 void f_pchar(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
