2ae3a0037b66fc8eca29fd309df0736775180af5
Shakir Abdo <shicolare1@gmail.com>
2023-11-17 00:50:13 +0300

    Implement the push and pall opcodes

--- monty.h
+++ monty.h
@@ -54,5 +54,5 @@ ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
-
+void f_pint(stack_t **head, unsigned int number);
 #endif
