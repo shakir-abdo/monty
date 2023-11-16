99e2069d720409094cbb43be851ab6b6c065bde2
Shakir Abdo <shicolare1@gmail.com>
2023-11-17 01:18:40 +0300

    Implement pop

--- monty.h
+++ monty.h
@@ -58,7 +58,7 @@ void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
-
+void f_pop(stack_t **head, unsigned int counter);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
