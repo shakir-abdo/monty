1b1df3ecb817e433c06488802c92e6c9cd92014f
Shakir Abdo <shicolare1@gmail.com>
2023-11-17 01:23:01 +0300

    Implement add

--- monty.h
+++ monty.h
@@ -60,6 +60,7 @@ int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
 void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
