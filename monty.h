410468cdc586f6be05179f36e6d7671fa4cd2a37
awab228 <awab228@yahoo.com>
2023-11-16 23:15:04 +0000

    Implement rotl

--- monty.h
+++ monty.h
@@ -68,6 +68,7 @@ void f_mul(stack_t **head, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
 void f_pchar(stack_t **head, unsigned int counter);
 void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
