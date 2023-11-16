babffbc4b836bffd04221aed7a2c7f423a8be8e0
awab228 <awab228@yahoo.com>
2023-11-16 23:22:06 +0000

    Implement stack and queue

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * f_stack - printing the top
+ * @head: stack head involve
+ * @counter: line_number involve
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
+
